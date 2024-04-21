d4d66196e84a5df7928eca9743dffbde0db0a702
Andrew Kinney <drewkinney03@gmail.com>
2024-04-21 00:01:59 -0400

    Initialize db connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 
 // find port
 const PORT = process.env.PORT || 8080;
