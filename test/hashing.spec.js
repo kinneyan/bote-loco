08db9843e2c0038447f95937a35cb7f0817e29e4
Andrew Kinney <drewkinney03@gmail.com>
2024-04-23 19:59:41 -0400

    Rework testing to not use other tested functions

--- test/hashing.spec.js
+++ test/hashing.spec.js
@@ -1,29 +1,33 @@
-const { expect } = require('chai');
-const { generateHash, compareHash } = require('../utils/hashing');
+const { expect } = require("chai");
+const { generateHash, compareHash } = require("../utils/hashing");
 
-describe('Hashing', () =>
+describe("Hashing", () =>
 {
+    // setup testing variables
     const password = "testpassword1234";
-    const exampleHash = generateHash(password);
-    describe('#generateHash()', () =>
+    const salt = "salt";
+    const expectedHash = "BqoRIIGX7M6pYxJ3vvzmWaZsqhcCGpLVdIhk0rukrrw2XpBRK+s2XwfX1o9G5egENc5SnKZ/pSoHrJHAii3Q3A==";
+    
+    describe("#generateHash()", () =>
     {
-        it('Hash should match test example using #compareHash()', () =>
+        it("Hash should match expected test hash", () =>
         {
-            const result = compareHash(password, exampleHash.salt, exampleHash.hash);
-            expect(result).to.equal(true);
+            const actual = generateHash(password, salt);
+            expect(actual.hash).to.equal(expectedHash);
         })
-    })
-    describe('#compareHash()', () =>
-    {
-        it('Should return false if hash does not match', () =>
-        {
-            const actual = compareHash('', exampleHash.salt, exampleHash.hash);
-            expect(actual).to.equal(false);
-        })
+    });
 
-        it('Should return true if hashes match', () =>
+    describe("#compareHash()", () =>
+    {
+        it("Should return false if hashed plaintext does not match hash", () =>
         {
-            expect(compareHash(password, exampleHash.salt, exampleHash.hash)).to.equal(true);
-        })
-    })
+            const actual = compareHash('', salt, expectedHash.hash);
+            expect(actual).to.equal(false);
+        });
+
+        it("Should return true if hashed plaintext matches hash", () =>
+        {
+            expect(compareHash(password, salt, expectedHash)).to.equal(true);
+        });
+    });
 });
