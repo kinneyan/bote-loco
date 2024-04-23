0776b3efc29d9ab0693fdcbc1bc0fe559ab86213
Andrew Kinney <drewkinney03@gmail.com>
2024-04-23 19:59:24 -0400

    Rework hashing to be testable

--- utils/hashing.js
+++ utils/hashing.js
@@ -1,25 +1,28 @@
-const { randomBytes, pbkdf2Sync } = require('crypto');
+const { randomBytes, pbkdf2Sync } = require("crypto");
 
 const ITERATIONS = 64000;
 const KEY_LEN = 64;
-const DIGEST = 'sha512';
+const DIGEST = "sha512";
 
 const generateSalt = () =>
 {
-    return randomBytes(128).toString('base64');
+    return randomBytes(128).toString("base64");
 }
 
-const generateHash = (password) =>
+const generateHash = (password, salt) =>
 {
-    const salt = generateSalt();
-    const hash = pbkdf2Sync(password, salt, ITERATIONS, KEY_LEN, DIGEST);
+    const hashed = pbkdf2Sync(password, salt, ITERATIONS, KEY_LEN, DIGEST);
+    return { salt: salt, hash: hashed.toString("base64") };
+}
 
-    return { salt: salt, hash: hash.toString('base64') };
+const hash = (password) =>
+{
+    return generateHash(password, generateSalt());
 }
 
 const compareHash = (password, salt, hash) =>
 {
-    return hash === pbkdf2Sync(password, salt, ITERATIONS, KEY_LEN, DIGEST).toString('base64');
+    return hash === pbkdf2Sync(password, salt, ITERATIONS, KEY_LEN, DIGEST).toString("base64");
 }
 
-module.exports = { generateHash, compareHash };
+module.exports = { hash, generateHash, compareHash };
