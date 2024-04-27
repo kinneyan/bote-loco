57a8ca4db81063fee9c8c0037e0f0294cba7c559
Andrew Kinney <drewkinney03@gmail.com>
2024-04-27 16:57:03 -0400

    Add register endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
 router.post("/login", require("../controllers/login").login);
+router.post("/register", require("../controllers/register").register);
 
 module.exports = router;
